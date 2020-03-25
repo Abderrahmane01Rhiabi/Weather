eed7f8ae6962b46db8d33329a2a1200bcf29f2c1
Abderrahman Rhiabi <rhiabi.abderrahmane.com>
2020-03-25 01:29:19 +0100

    fotry commit

--- models/capteurs.js
+++ models/capteurs.js
@@ -3,10 +3,8 @@ const mongoose = require('mongoose');
 var capteurSchema = new mongoose.Schema({
     macAddr : {type : String, required : true ,unique : true},
     name : {type : String, required : true},
-    place : [{
-         x : {type : String, required : true },
-         y : {type : String, required : true }
-    }]
+         place_x : {type : String, required : true },
+         place_x : {type : String, required : true }
 })
 
 capteurSchema.pre('save',  function(next) {
