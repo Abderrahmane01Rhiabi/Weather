03ce398ec60cf452d8f61dde1960f8d64e94fe07
Abderrahman Rhiabi <rhiabi.abderrahmane.com>
2020-03-25 23:46:00 +0100

    fivety commit

--- routes/capteur/pageCapteur.js
+++ routes/capteur/pageCapteur.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const verifyToken = require('../../middleware/verifyToken')
 var _ = require('lodash');
+var mqtt = require('mqtt');
+const thingsboardHost = "demo.thingsboard.io";
+// Reads the access token from arguments
+const accessToken = "WHWNzii8l9CdXUhAdVAO";
 
 const secret = "secret";
 
@@ -150,70 +154,80 @@ router.post('/weatherData/:macAddCapt',(req,res) =>{
                 });
             }
             else{
-                const minTemperature = 17.5, maxTemperature = 30, minHumidity = 12, maxHumidity = 90;
+                    const minTemperature = 17.5, maxTemperature = 30, minHumidity = 12, maxHumidity = 90;
 
-                // Initialization of temperature and humidity data with random values
-                var data = {
-                    temperature: minTemperature + (maxTemperature - minTemperature) * Math.random() ,
-                    humidity: minHumidity + (maxHumidity - minHumidity) * Math.random()
-                };
+                    // Initialization of temperature and humidity data with random values
+                    var data = {
+                        temperature: minTemperature + (maxTemperature - minTemperature) * Math.random() ,
+                        humidity: minHumidity + (maxHumidity - minHumidity) * Math.random()
+                    };
+
+                    // Initialization of mqtt client using Thingsboard host and device access token
+                    console.log('Connecting to: %s using access token: %s', thingsboardHost, accessToken);
+                    var client  = mqtt.connect('mqtt://'+ thingsboardHost, { username: accessToken });
 
 
-                // Triggers when client is successfully connected to the Thingsboard server
-                client.on('connect', function () {
-                    console.log('Client connected!');
-                    console.log('Uploading temperature and humidity data once per second...');
-                    setInterval(publishTelemetry, 1000);
-                });
+                    // Triggers when client is successfully connected to the Thingsboard server
+                    client.on('connect', function () {
+                        console.log('Client connected!');
+                        client.publish('v1/devices/me/attributes', JSON.stringify({"firmware_version":"1.0.1"}));
+                        console.log('Uploading temperature and humidity data once per second...');
+                        setInterval(publishTelemetry, 1000);
+                    });
 
-                // Uploads telemetry data using 'v1/devices/me/telemetry' MQTT topic
-                function publishTelemetry() {
-                    data.temperature = genNextValue(data.temperature, minTemperature, maxTemperature);
-                    data.humidity = genNextValue(data.humidity, minHumidity, maxHumidity);
-                    console.log('1')   
-                    //client.publish('v1/devices/me/telemetry', JSON.stringify(data));
-                    // console.log('v1/devices/me/telemetry', JSON.stringify(data))
-                    var d = {
-                        "temp" : data.temperature,
-                        "humidite" : data.humidity,
-                        "macAddCapt" : req.params.macAddCapt
-                    }
-                    var newData = new Weather(d)
-                    newData.save()
-                    .then(data => {
-                        res.status(200).json({
-                            message : "data arrive"
+                    // Uploads telemetry data using 'v1/devices/me/telemetry' MQTT topic
+                    function publishTelemetry() {
+                        data.temperature = genNextValue(data.temperature, minTemperature, maxTemperature);
+                        data.humidity = genNextValue(data.humidity, minHumidity, maxHumidity);
+                        client.publish('v1/devices/me/telemetry', JSON.stringify(data));
+                        console.log('v1/devices/me/telemetry', JSON.stringify(data))
+                        var d = {
+                            "temp" : data.temperature,
+                            "humidite" : data.humidity,
+                            "macAddCapt" : req.params.macAddCapt
+                        }
+                         var newData = new Weather(d)
+                         newData.save()
+                         .then(err => {
+                            if(err) return err; 
+                             res.status(200).json({
+                                 message : "data arrive"
+                             })
                         })
-                    }).catch(err => {
-                            console.log(err);
-                            res.status(500).json({
-                                error : err
-                            });
+                        .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error : err
+                                });
                             })
-                }
 
-                // Generates new random value that is within 3% range from previous value
-                function genNextValue(prevValue, min, max) {
-                    var value = prevValue + ((max - min) * (Math.random() - 0.5)) * 0.03;
-                    value = Math.max(min, Math.min(max, value));
-                    return Math.round(value * 10) / 10;
-                }
+                    }
 
-                //Catches ctrl+c event
-                process.on('SIGINT', function () {
-                    console.log();
-                    console.log('Disconnecting...');
-                    client.end();
-                    console.log('Exited!');
-                    process.exit(2);
-                });
+                    // Generates new random value that is within 3% range from previous value
+                    function genNextValue(prevValue, min, max) {
+                        var value = prevValue + ((max - min) * (Math.random() - 0.5)) * 0.03;
+                        value = Math.max(min, Math.min(max, value));
+                        return Math.round(value * 10) / 10;
+                    }
 
-                //Catches uncaught exceptions
-                process.on('uncaughtException', function(e) {
-                    console.log('Uncaught Exception...');
-                    console.log(e.stack);
-                    process.exit(99);
-                });
+                    //Catches ctrl+c event
+                    process.on('SIGINT', function () {
+                        console.log();
+                        console.log('Disconnecting...');
+                        client.end();
+                        // res.status(200).json({
+                        //     message : "data arrive"
+                        // })
+                        console.log('Exited!');
+                        process.exit(2);
+                    });
+
+                    //Catches uncaught exceptions
+                    process.on('uncaughtException', function(e) {
+                        console.log('Uncaught Exception...');
+                        console.log(e.stack);
+                        process.exit(99);
+                    });
                 // var data = {
                 //     "macAddCapt" : req.params.macAddCapt,
                 //     "temp" : req.params.temp,
@@ -222,7 +236,7 @@ router.post('/weatherData/:macAddCapt',(req,res) =>{
                 // var newData = new Weather(data)
             }
     }).catch(err =>{
-        return res.status(500).json({
+         res.status(500).json({
             error : err
         });
     })
